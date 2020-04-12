b784910e1cce895afd919b8e1c656aa3e813c814
yasuki doukita <yasukidoukita1@gmail.com>
2020-04-12 11:46:05 +0900

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,8 +29,7 @@ class ArticlesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /articles/1
-  # PATCH/PUT /articles/1.json
+
   def update
     respond_to do |format|
       if @article.update(article_params)
